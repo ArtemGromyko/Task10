70a24bd418b48a59e4b810a7e54a68589de0f579
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-10-26 18:51:49 +0300

    Добавьте файлы проекта.

---
+++ Task10/Book.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+
+namespace Task10
+{
+    class Book
+    {
+        public string Name { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Author { get; set; }
+        public Book() { }
+        public Book(string name, int number, string author)
+        {
+            Name = name;
+            NumberOfPages = number;
+            Author = author;
+        }
+        public void Disp()
+        {
+            Console.WriteLine("Name: "+Name);
+            Console.WriteLine("NumberOfPages: "+NumberOfPages);
+            Console.WriteLine("Author: "+Author);
+        }
+        public void ReadObjectFromXML(string path)
+        {
+            XmlDocument xml = new XmlDocument();
+            try
+            {
+                xml.Load(path);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            XmlElement root = xml.DocumentElement;
+            foreach(XmlNode x in root.ChildNodes)
+            {
+                if (x.Name == "Name")
+                    Name = x.InnerText;
+                else if (x.Name == "NumberOfPages")
+                    NumberOfPages = Int32.Parse(x.InnerText);
+                else if (x.Name == "Author")
+                    Author = x.InnerText;
+            }
+        }
+        public void WriteObjectToXML(string path)
+        {
+            XmlDocument xml = new XmlDocument();
+            try
+            {
+                xml.Load(path);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            XmlElement root = xml.DocumentElement;
+
+            XmlElement xBook = xml.CreateElement("Book");
+
+            XmlElement xName = xml.CreateElement("Name");
+            XmlText xNameText = xml.CreateTextNode(this.Name);
+            xName.AppendChild(xNameText);
+
+            XmlElement xNumber = xml.CreateElement("NumberOfPages");
+            XmlText xNumberText = xml.CreateTextNode(this.NumberOfPages.ToString());
+            xNumber.AppendChild(xNumberText);
+
+            XmlElement xAuthor = xml.CreateElement("Author");
+            XmlText xAuthorText = xml.CreateTextNode(this.Author);
+            xAuthor.AppendChild(xAuthorText);
+
+            xBook.AppendChild(xName);
+            xBook.AppendChild(xNumber);
+            xBook.AppendChild(xAuthor);
+
+            root.AppendChild(xBook);
+
+            xml.Save(path);
+        }
+        public static void WriteObjectToJSON(string path, Book b)
+        {
+            string s = JsonSerializer.Serialize<Book>(b);
+            using (StreamWriter sw = new StreamWriter(path, false))
+                sw.Write(s);
+        }
+        public static Book ReadObjectFromJSON(string path)
+        {
+            string s = File.ReadAllText(path);
+            Book b = new Book();
+            b = JsonSerializer.Deserialize<Book>(s);
+            return b;
+        }
+    }
+}
---
+++ Task10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            b.ReadObjectFromXML(@"..\..\..\XMLBookRead.xml");
+            b.Disp();
+            b.WriteObjectToXML(@"..\..\..\XMLBookWrite.xml");
+            Book.WriteObjectToJSON(@"..\..\..\write.json", b);
+            Book a = new Book();
+            a = Book.ReadObjectFromJSON(@"..\..\..\read.json");
+            Console.WriteLine("a: ");
+            a.Disp();
+            Book.WriteObjectToJSON(@"..\..\..\write.json", a);
+
+        }
+    }
+}
